1da52b6d29fb5f4e9f529853003fc60c7fa37b5f
osher cohen <oscohen@localhost.localdomain>
2021-05-04 22:19:39 +0300

    Enrich jira when mentiond in ticket

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # keep-it-simple-bot
 
 * Inform in a thread the message is too long
-
+* Enrich jira title when ticket is mentioned  
 # Usage:
 ```
 SLACK_BOT_TOKEN: slack bot token
 SLACK_APP_TOKEN: slack app token
 MANAGER (optional): manager name to mention in the comment, default "the manager"
 MAX_MESSAGE_LEN (optional): max len of the message, default 500
+JIRA_CREDS (optional): jira creds with the format of 'user:password'
 ```
--- keep_it_simple.py
+++ keep_it_simple.py
@@ -1,4 +1,6 @@
 import os
+import re
+import jira
 import CONSTS
 
 from slack_bolt import App
@@ -9,6 +11,15 @@ app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 MANAGER = os.environ.get("MANAGER", "the manager")
 MAX_MESSAGE_LEN = os.environ.get("MAX_MESSAGE_LEN", CONSTS.MAX_MESSAGE_LEN)
 
+JIRA_SERVER = "https://issues.redhat.com"
+jira_creds = os.environ.get("JIRA_CREDS", None)
+if jira_creds:
+    jira_user, jira_pass = jira_creds.split(":")
+    jira_client = jira.JIRA(JIRA_SERVER, basic_auth=(jira_user, jira_pass))
+else:
+    jira_user = None
+
+
 # Check liveness
 @app.event("app_mention")
 def event_test(say):
@@ -17,13 +28,44 @@ def event_test(say):
 @app.event("message")
 def make_it_short(client, message):
     channel_id = message["channel"]
-    if message["channel_type"] in ["group", "channel"] and "thread_ts" not in message and len(message["text"]) > CONSTS.MAX_MESSAGE_LEN:
+    keep_it_short(channel_id, client, message)
+    enrich_jira_mention(channel_id, client, message)
+
+
+def keep_it_short(channel_id, client, message):
+    if message["channel_type"] in ["group", "channel"] and "thread_ts" not in message and len(
+            message["text"]) > CONSTS.MAX_MESSAGE_LEN:
         message_len = len(message["text"])
         result = client.chat_postMessage(
             channel=channel_id,
             thread_ts=message["ts"],
-            text=CONSTS.MAX_MESSAGE_COMMENT.format(max_message_len=CONSTS.MAX_MESSAGE_LEN, message_len=message_len, manager=MANAGER)
+            text=CONSTS.MAX_MESSAGE_COMMENT.format(max_message_len=CONSTS.MAX_MESSAGE_LEN, message_len=message_len,
+                                                   manager=MANAGER)
         )
+
+
+def enrich_jira_mention(channel_id, client, message):
+    if not jira_user:
+        return
+    jira_links = re.findall("<https:\/\/issues\.redhat\.com\/browse\/MGMT-.*?>", message["text"])
+    if not jira_links:
+        return
+    new_message = message["text"]
+    for link in jira_links:
+        ticket_description = get_jira_info(link)
+        link_with_description = f"{link} \n> :jira: {ticket_description}\n"
+        new_message = new_message.replace(link, link_with_description)
+    client.chat_update(
+        text=new_message,
+        channel=channel_id,
+        ts=message["ts"]
+    )
+
+def get_jira_info(ticket):
+    ticket_key = re.findall("MGMT-.*[1-9]",ticket)[0]
+    issue = jira_client.search_issues(f"key={ticket_key}")[0]
+    title = issue.raw['fields']['summary']
+    return title
 
 if __name__ == "__main__":
     SocketModeHandler(app, os.environ.get("SLACK_APP_TOKEN")).start()
